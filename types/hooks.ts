9ce6a198bb787bb73a90fd2ff93840d9a1d72c4e
Songonha <105191398+songonha@users.noreply.github.com>
2023-09-11 22:37:13 +0700

    Add files via upload

---
+++ types/hooks.ts
@@ -0,0 +1,23 @@
+
+
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import { Contract, providers } from "ethers"
+import { SWRResponse } from "swr";
+import { SaigonWalletContract } from "./SaigonWalletContract";
+
+
+export type Web3Dependencies = {
+  provider: providers.Web3Provider;
+  contract: SaigonWalletContract;
+  ethereum: MetaMaskInpageProvider;
+  isLoading: boolean;
+}
+
+export type CryptoHookFactory<D = any, R = any, P = any> = {
+  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, R, P>
+}
+
+export type CryptoHandlerHook<D = any, R = any, P = any> = (params?: P) => CryptoSWRResponse<D, R>
+
+export type CryptoSWRResponse<D = any, R = any> = SWRResponse<D> & R;
+
---
+++ types/nft.ts
@@ -0,0 +1,39 @@
+
+export type Trait = "Year" | "Origin" | "Artisan";
+
+export type NftAttribute = {
+  trait_type: Trait;
+  value: string;
+}
+
+export type NftMeta = {
+  name: string;
+  description: string;
+  image: string;
+  attributes: NftAttribute[];
+}
+
+export type NftCore = {
+  bonsaiId: number;
+  price: number;
+  creator: string;
+  isListed: boolean
+}
+
+export type Nft = {
+  meta: NftMeta
+} & NftCore
+
+
+export type FileReq = {
+  bytes: Uint8Array;
+  contentType: string;
+  fileName: string;
+}
+
+export type PinataRes = {
+  IpfsHash: string;
+  PinSize: number;
+  Timestamp: string;
+  isDuplicate: boolean;
+}
